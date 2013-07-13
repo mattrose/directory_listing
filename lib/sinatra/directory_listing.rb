18fa0af4b3cf56581c9a5a4623cdd9fbab37181b
Richard Myers <rick.myers@me.com>
2013-07-13 16:33:57 -0400

    reorganize

--- lib/sinatra/directory_listing.rb
+++ lib/sinatra/directory_listing.rb
@@ -73,6 +73,49 @@ require 'uri'
 module Sinatra
   module Directory_listing
     
+    def list(o={})
+      options = {
+        :should_list_invisibles => "no",
+        :last_modified_format => "%Y-%m-%d %H:%M:%S",
+        :filename_truncate_length => 40,
+        :stylesheet => "",
+        :readme => ""
+      }.merge(o)
+      
+      $should_list_invisibles = options[:should_list_invisibles]
+      $last_modified_format = options[:last_modified_format]
+      $filename_truncate_length = options[:filename_truncate_length]
+      
+      html = "<html>\n<head>\n"
+      html << "<title>Index of #{URI.unescape(request.path)}</title>\n"
+      html << "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>\n"
+      if options[:stylesheet]
+        html << "<link rel=\"stylesheet\" type=\"text/css\" href=\"/#{options[:stylesheet].sub(/^[\/]*/,"")}\">\n"
+      end
+      html << "</head>\n<body>\n"
+      html << "<h1>Index of #{URI.unescape(request.path)}</h1>\n"
+      if URI.unescape(request.path) != "/"
+        html << "<a href='#{Pathname.new(URI.unescape(request.path)).parent}'>&larr; Parent directory</a><br><br>"
+      else
+        html << "<a>Root directory</a><br><br>"
+      end
+      html << "<table>\n"
+      html << "\t<tr>\n\t\t<th>File</th>\n\t\t<th>Last modified</th>\n\t\t<th>Size</th>\n\t</tr>\n"
+      files = Array.new
+      Dir.foreach(File.join(settings.public_folder, URI.unescape(request.path)), &files.method(:push))
+      if files == [".", ".."]
+        html << "\t<tr>\n\t\t<th>No files.</th>\n\t\t<th>-</th>\n\t\t<th>-</th>\n\t</tr>"
+      else
+        files.sort.each do |file|
+          html << self.wrap(file)
+        end
+      end
+      html << "\n</table>\n"
+      html << "<br>\n#{options[:readme]}\n" if options[:readme]
+      html << "</body>\n</html>\n"
+      html
+    end
+    
     def m_time(file)
       f = File.join(File.join(settings.public_folder, URI.unescape(request.fullpath)), file)
       "\t<td>#{File.mtime(f).strftime $last_modified_format}</td>"
@@ -110,64 +153,19 @@ module Sinatra
       html = ""
       if $should_list_invisibles == "yes"
         html << "\n\t<tr>
-        #{name(file)}
-        #{m_time(file)}
-        #{size(file)}\n\t</tr>"
+        #{self.name(file)}
+        #{self.m_time(file)}
+        #{self.size(file)}\n\t</tr>"
       else
         if file[0] != "."
           html << "\n\t<tr>
-          #{name(file)}
-          #{m_time(file)}
-          #{size(file)}\n\t</tr>"
+          #{self.name(file)}
+          #{self.m_time(file)}
+          #{self.size(file)}\n\t</tr>"
         end
       end
       html
     end
-            
-    def list(o={})
-      options = {
-        :should_list_invisibles => "no",
-        :last_modified_format => "%Y-%m-%d %H:%M:%S",
-        :filename_truncate_length => 40,
-        :stylesheet => "",
-        :readme => ""
-      }.merge(o)
-      
-      $should_list_invisibles = options[:should_list_invisibles]
-      $last_modified_format = options[:last_modified_format]
-      $filename_truncate_length = options[:filename_truncate_length]
-      
-      html = "<html>\n<head>\n"
-      html << "<title>Index of #{URI.unescape(request.path)}</title>\n"
-      html << "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>\n"
-      if options[:stylesheet]
-        html << "<link rel=\"stylesheet\" type=\"text/css\" href=\"/#{options[:stylesheet].sub(/^[\/]*/,"")}\">\n"
-      end
-      html << "</head>\n<body>\n"
-      html << "<h1>Index of #{URI.unescape(request.path)}</h1>\n"
-      if URI.unescape(request.path) != "/"
-        html << "<a href='#{Pathname.new(URI.unescape(request.path)).parent}'>&larr; Parent directory</a><br><br>"
-      else
-        html << "<a>Root directory</a><br><br>"
-      end
-      html << "<table>\n"
-      html << "\t<tr>\n\t\t<th>File</th>\n\t\t<th>Last modified</th>\n\t\t<th>Size</th>\n\t</tr>\n"
-      files = Array.new
-      Dir.foreach(File.join(settings.public_folder, URI.unescape(request.path)), &files.method(:push))
-      if files == [".", ".."]
-        html << "\t<tr>\n\t\t<th>No files.</th>\n\t\t<th>-</th>\n\t\t<th>-</th>\n\t</tr>"
-      else
-        files.sort.each do |file|
-          html << wrap(file)
-        end
-      end
-      html << "\n</table>\n"
-      html << "<br>\n#{options[:readme]}\n" if options[:readme]
-      html << "</body>\n</html>\n"
-      html
-    end
-    
-    private :wrap, :name, :size, :m_time
       
   end
 
