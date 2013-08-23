ed5529a71a857b9fd7d9b3e7b162eb229ae1e42f
Richard Myers <rick.myers@me.com>
2013-08-23 08:28:03 -0400

    make code a bit more readable

--- lib/sinatra/directory_listing.rb
+++ lib/sinatra/directory_listing.rb
@@ -5,14 +5,20 @@ require 'pathname'
 require 'uri'
 require 'erb'
 
-# = Easy, CSS-styled, Apache-like directory listings for Sinatra.
-#
-# == Usage
-#
-# list() will return HTML, so the following is a complete Sinatra 
-# app that will provide a directory listing of whatever path you navigate to
-# and let you view any file that is served directly:
-#
+# ### directory_listing: easy, CSS-styled, Apache-like directory listings for Sinatra.
+# 
+# ### Build from source:
+# 
+# ```bash
+# gem build directory_listing.gemspec
+# sudo gem install ./directory_listing-x.x.x.gem
+# ```
+# 
+# ### Usage:
+# 
+# ```list()``` will return HTML, so the following is a complete Sinatra app that will provide a directory listing of whatever path you navigate to and let you view any file that is served directly:
+# 
+# ```ruby
 # require 'sinatra'
 # require 'sinatra/directory_listing'
 # 
@@ -31,45 +37,55 @@ require 'erb'
 # not_found do
 #   'Try again.'
 # end
-#
-# == Options
+# ```
+# 
+# ### Options:
 # 
 # Options are passed in a hash:
 # 
+# ```ruby
 # list({
 #   :stylesheet => "stylesheets/styles.css",
 #   :readme => "<a>Welcome!</a>"
 # })
-#
-# Available options:
-#
-# stylesheet # a stylesheet that will be added to the <head> of the generated directory listing
-# readme # an HTML string that will be appended at the footer of the generated directory listing
-# should_list_invisibles # whether the directory listing should include invisibles (dotfiles) - true or false, defaults to false
-# last_modified_format # format for last modified date (http://www.ruby-doc.org/core-2.0/Time.html) - defaults to "%Y-%m-%d %H:%M:%S"
-# filename_truncate_length # (integer) length to truncate file names to - defaults to 40
-#
-# == Styling
-#
-# It's pretty easy to figure out how to style directory_listing by looking at the source, but here are some gotchas:
-#
-# Every item listed is a <td> element in a table. Directories will have a class of "dir" and regular files will have a class of "file". 
-#
-# You can style the "File" column with this CSS:
+# ```
 # 
+# Available options:
+# 
+# - ```stylesheet``` - a stylesheet that will be added to the <head> of the generated directory listing
+# - ```readme``` - an HTML string that will be appended at the footer of the generated directory listing
+# - ```should_list_invisibles``` - whether the directory listing should include invisibles (dotfiles) - true or false, defaults to false
+# - ```last_modified_format``` - [format](http://www.ruby-doc.org/core-2.0/Time.html) for last modified date - defaults to ```%Y-%m-%d %H:%M:%S```
+# - ```filename_truncate_length``` - (integer) length to truncate file names to - defaults to 40
+# 
+# ### Styling:
+# 
+# It's pretty easy to figure out how to style ```directory_listing``` by looking at the source, but here are some gotchas:
+# 
+# - Every item listed is a ```<td>``` element in a table. Directories will have a class of ```dir``` and regular files will have a class of ```file```. 
+# - You can style the "File" column with this CSS:
+# 
+# ```css
 # table tr > td:first-child { 
 #   text-align: left;
 # }
+# ```
 # 
-# Second column:
+# - "Last modified" column:
+# 
+# ```css
 # table tr > td:first-child + td { 
 #   text-align: left;
 # }
+# ```
 # 
-# Third column:
+# - "Size" column:
+# 
+# ```css
 # table tr > td:first-child + td + td { 
 #   text-align: left;
 # }
+# ```
 
 module Sinatra
   module Directory_listing
@@ -126,7 +142,7 @@ module Sinatra
     end
     
     ##
-    # Get the name of a file. 
+    # Get the name of the file and its link.
     
     def name(file)
       file = URI.unescape(file)
@@ -139,10 +155,11 @@ module Sinatra
     
       html = ""
       if File.directory?(File.join(settings.public_folder, link))
-        html << "\t<td class='dir'><a href='#{link}'>#{tfile}</a></td>"
+        html << "\t<td class='dir'>"
       else
-        html << "\t<td class='file'><a href='#{link}'>#{tfile}</a></td>"
+        html << "\t<td class='file'>"
       end
+      html << "<a href='#{link}'>#{tfile}</a></td>"
       html
     end
     
@@ -200,7 +217,12 @@ module Sinatra
       files = Array.new
       Dir.foreach(File.join(settings.public_folder, URI.unescape(request.path)), &files.method(:push))
       if files == [".", ".."]
-        $files_html << "\t<tr>\n\t\t<th>No files.</th>\n\t\t<th>-</th>\n\t\t<th>-</th>\n\t</tr>"
+        $files_html << "
+          <tr>
+            <th>No files.</th>
+            <th>-</th>
+            <th>-</th>
+          </tr>"
       else
         files.sort.each do |file|
           $files_html << self.wrap(file)
